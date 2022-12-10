30415ae421a2a8499df86d89f904a4c4045157c9
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-10 17:48:57 +0200

    add loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { MaterialEditor } from './MaterialEditor';
+import { addMaterial } from 'servise/api';
+
+export class App extends Component {
+  state = {
+    materials: [],
+    isLoading: false,
+  };
+
+  addMaterial = async values => {
+    try {
+      this.setState({ isLoading: true });
+      const material = await addMaterial(values);
+      this.setState(prevState => ({
+        materials: [...prevState.materials, material],
+        isLoading: false,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <>
+        {isLoading && <div>Loading...</div>}
+        <MaterialEditor onSubmit={this.addMaterial} />
+      </>
+    );
+  }
+}
---
+++ src/components/MaterialEditor.jsx
@@ -0,0 +1,29 @@
+import { Formik, Form, Field } from 'formik';
+
+export const MaterialEditor = ({ onSubmit, isSubmitting }) => {
+  const handleSubmit = async (values, actions) => {
+    await onSubmit(values);
+    actions.setSubmitting(false);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik initialValues={{ title: '', link: '' }} onSubmit={handleSubmit}>
+      {({ isSubmitting }) => (
+        <Form>
+          <label>
+            Опис
+            <Field name="title" type="text" />
+          </label>
+          <label>
+            Посилання
+            <Field name="link" type="text"></Field>
+          </label>
+          <button type="submit" disabled={isSubmitting}>
+            Добавити матеріал
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
